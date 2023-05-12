c007a3292bb1d6aa119485df8140103ad2615756
Akas_Datta <012akasdatta@gmail.com>
2023-05-13 02:37:34 +0600

    added delete button and modified some component and card

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,53 @@
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({coffee}) => {
+    const {_id, name, quantity, supplier, taste, category, details, photo} = coffee;
+
+    const handleDelete = _id =>{
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+            //   Swal.fire(
+            //     'Deleted!',
+            //     'Your file has been deleted.',
+            //     'success'
+            //   )
+
+            console.log('delete confirm')
+            }
+          })
+    }
+    return (
+        <div>
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img src={photo} alt="Movie"/></figure>
+                <div className="flex justify-between w-full pr-4">
+                    <div>
+                        <h2 className="card-title">Name: {name}</h2>
+                        <p>{quantity}</p>
+                        <p>{supplier}</p>
+                        <p>{taste}</p>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <div className="btn-group btn-group-vertical space-y-4">
+                            <button className="btn">View</button>
+                            <button className="btn">Edit</button>
+                            <button
+                            onClick={() => handleDelete(_id)} className="btn bg-orange-600">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeCard;
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,15 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import AddCoffee from './comonents/AddCoffee';
-import UpdateCoffee from './comonents/UpdateCoffee';
+import AddCoffee from './components/AddCoffee';
+import UpdateCoffee from './components/UpdateCoffee';
 import App from './App';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
   },
   {
     path: "addCoffee",
