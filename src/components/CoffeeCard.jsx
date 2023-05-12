9bdd0f405e696b7d8e8de81265ef3817c419ce60
Akas_Datta <012akasdatta@gmail.com>
2023-05-13 02:57:18 +0600

    some file modified

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,30 +1,36 @@
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const CoffeeCard = ({coffee}) => {
     const {_id, name, quantity, supplier, taste, category, details, photo} = coffee;
-
-    const handleDelete = _id =>{
+    const handleDelete = _id => {
         console.log(_id);
+      
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-            //   Swal.fire(
-            //     'Deleted!',
-            //     'Your file has been deleted.',
-            //     'success'
-            //   )
-
-            console.log('delete confirm')
-            }
-          })
-    }
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then(result => {
+          if (result.isConfirmed) {
+            fetch(`http://localhost:5000/coffee/${_id}`, {
+              method: 'DELETE'
+            })
+              .then(res => res.json())
+              .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                  Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+                }
+              });
+          }
+        });
+      };
+      
+    
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl">
@@ -39,7 +45,9 @@ const CoffeeCard = ({coffee}) => {
                     <div className="card-actions justify-end">
                         <div className="btn-group btn-group-vertical space-y-4">
                             <button className="btn">View</button>
-                            <button className="btn">Edit</button>
+                            <Link to={`updateCoffee/${_id}`}>
+                                <button className="btn">Edit</button>
+                            </Link>
                             <button
                             onClick={() => handleDelete(_id)} className="btn bg-orange-600">Delete</button>
                         </div>
